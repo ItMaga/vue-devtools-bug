0128d2e4fbc9651a3f1234e89cb3364b94f3fffa
ItMaga <m-chemurziev@ya.ru>
2021-11-18 19:26:43 +0300

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VueDraggable :list="list1" group="test" :noTransitionOnDrag="true">
+      <transition-group name="card-transition">
+        <Card v-for="item of list1" :key="item.id" :title="item.title" />
+      </transition-group>
+    </VueDraggable>
+    <VueDraggable :list="list2" group="test" :noTransitionOnDrag="true">
+      <transition-group name="card-transition">
+        <Card v-for="item of list2" :key="item.id" :title="item.title" />
+      </transition-group>
+    </VueDraggable>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueDraggable from 'vuedraggable';
+import Card from "@/components/Card";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  components: {Card, VueDraggable },
+
+  data: () => ({
+    list1: [
+      {
+        id: 1,
+        title: 'Item 1',
+      },
+      {
+        id: 2,
+        title: 'Item 2',
+      }
+    ],
+    list2: [
+      {
+        id: 3,
+        title: 'Item 3',
+      },
+      {
+        id: 4,
+        title: 'Item 4',
+      }
+    ]
+  })
 }
 </script>
 
 <style>
 #app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: 50px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.card-transition-move {
+  transition: transform 0.3s;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ['title'],
+}
+</script>
+
+<style scoped>
+.card {
+  margin-bottom: 10px;
+  padding: 10px;
+  background-color: gray;
+  cursor: pointer;
+}
+</style>
